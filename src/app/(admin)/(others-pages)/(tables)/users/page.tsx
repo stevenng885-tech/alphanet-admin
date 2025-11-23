626877b3e2bd473816d35183080ad38b18251dea
stevenng885-tech <stevenng885@gmail.com>
2025-11-23 15:24:44 +0700

    v1.0.0

--- src/app/(admin)/(others-pages)/(tables)/users/page.tsx
+++ src/app/(admin)/(others-pages)/(tables)/users/page.tsx
@@ -8,7 +8,6 @@ export const metadata: Metadata = {
   title: "Next.js Basic Table | TailAdmin - Next.js Dashboard Template",
   description:
     "This is Next.js Basic Table  page for TailAdmin  Tailwind CSS Admin Dashboard Template",
-  // other metadata
 };
 
 export default function BasicTables() {
--- src/components/tables/BasicTableOne.tsx
+++ src/components/tables/BasicTableOne.tsx
@@ -12,8 +12,15 @@ import { firebaseFireStore } from "../../../utils/shared/firebase";
 import { collection, getDocs } from "firebase/firestore";
 import { FaUser } from "react-icons/fa";
 
+type TypeOrder = {
+  id: string,
+  name: string,
+  phone: string,
+  email: string,
+}
+
 export default function BasicTableOne() {
-  const [usersContact, setUserContact] = React.useState()
+  const [usersContact, setUserContact] = React.useState<Array<TypeOrder>>([])
 
   React.useEffect(() => {
     (async () => {
@@ -22,7 +29,7 @@ export default function BasicTableOne() {
         if (!querySnapshot.empty) {
           const contacts = querySnapshot.docs.map((doc) => {
             return {
-              ...doc.data(),
+              ...doc.data() as TypeOrder,
               id: doc.id
             }
           });
@@ -38,12 +45,12 @@ export default function BasicTableOne() {
   console.log(usersContact)
 
   return (
-    <div className="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-white/[0.05] dark:bg-white/[0.03]">
-      <div className="max-w-full overflow-x-auto">
-        <div className="min-w-[1102px]">
+    <div className="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-white/[0.05] dark:bg-white/[0.03]" >
+      <div className="max-w-full overflow-x-auto" >
+        <div className="min-w-[1102px]" >
           <Table>
             {/* Table Header */}
-            <TableHeader className="border-b border-gray-100 dark:border-white/[0.05]">
+            < TableHeader className="border-b border-gray-100 dark:border-white/[0.05]" >
               <TableRow>
                 <TableCell
                   isHeader
@@ -51,13 +58,13 @@ export default function BasicTableOne() {
                 >
                   User Name
                 </TableCell>
-                <TableCell
+                < TableCell
                   isHeader
                   className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
                 >
                   Phone Number
                 </TableCell>
-                <TableCell
+                < TableCell
                   isHeader
                   className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
                 >
@@ -67,36 +74,37 @@ export default function BasicTableOne() {
             </TableHeader>
 
             {/* Table Body */}
-            <TableBody className="divide-y divide-gray-100 dark:divide-white/[0.05]">
-              {usersContact.map((order: any) => (
-                <TableRow key={order.id}>
-                  <TableCell className="px-5 py-4 sm:px-6 text-start">
-                    <div className="flex items-center gap-3">
-                      <div className="w-10 h-10 overflow-hidden rounded-full flex justify-center items-center">
+            <TableBody className="divide-y divide-gray-100 dark:divide-white/[0.05]" >
+              {usersContact && usersContact.map((order: TypeOrder) => (
+                <TableRow key={order.id} >
+                  <TableCell className="px-5 py-4 sm:px-6 text-start" >
+                    <div className="flex items-center gap-3" >
+                      <div className="w-10 h-10 overflow-hidden rounded-full flex justify-center items-center" >
                         <FaUser
                           className="text-white"
                           width={40}
                           height={40}
                         />
                       </div>
-                      <div>
-                        <span className="block font-medium text-gray-800 text-theme-sm dark:text-white/90">
+                      < div >
+                        <span className="block font-medium text-gray-800 text-theme-sm dark:text-white/90" >
                           {order.name}
                         </span>
-                        <span className="block text-gray-500 text-theme-xs dark:text-gray-400">
+                        < span className="block text-gray-500 text-theme-xs dark:text-gray-400" >
                           Web Data
                         </span>
                       </div>
                     </div>
                   </TableCell>
-                  <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
+                  < TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400" >
                     {order.phone}
                   </TableCell>
-                  <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
+                  < TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400" >
                     {order.email}
                   </TableCell>
                 </TableRow>
-              ))}
+              ))
+              }
             </TableBody>
           </Table>
         </div>
