440077201fa9d260c3669dfe3f309c581b6fe104
stevenng885-tech <stevenng885@gmail.com>
2025-12-13 17:22:35 +0700

    minor change

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,7 +2,7 @@ import { clerkClient } from "@clerk/nextjs/server";
 
 export async function GET() {
   const client = await clerkClient()
-  const users = await client.users.getUserList();
+  const users = await client.users.getUserList({ limit: 100 });
 
   return Response.json(users);
 }
